daf51710526467d793e62b3fc8cf6f493145f448
alisson santana barbosa <alisson98sb@gmail.com>
2026-05-06 16:56:10 -0300

    Day 1 finished

---
+++ Day 1/tasks/task.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator")
+city = input("What city are you in? \n")
+animal = input("What animal are you in? \n")
+
+print("Your band name could be: " + city + " " + animal)
